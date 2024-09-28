4c1febe98adf232b6ec89bb1d5c58fc37d576d81
Amiri <amirymax@gmail.com>
2024-09-28 15:54:21 +0300

    first commit

---
+++ musker/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
+
+# Unregister Groups
+admin.site.unregister(Group)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+# Extend User model
+class UserAdmin(admin.ModelAdmin):
+    user = User
+
+    fields = ['username']
+    inlines = [ProfileInline]
+
+# Unregister initial user
+admin.site.unregister(User)
+
+# Reregister user and Profile
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
+
