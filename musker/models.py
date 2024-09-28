51bf5b170e71ec11c9f4e44a9ab395e09165efce
Amiri <amirymax@gmail.com>
2024-09-28 23:39:17 +0300

    Finished 4th lesson

--- musker/models.py
+++ musker/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
 
 # Create A User Profile Model
 class Profile(models.Model):
@@ -9,6 +9,16 @@ class Profile(models.Model):
                                      related_name="followed_by",
                                      symmetrical=False,
                                      blank=True)
+    date_modified = models.DateTimeField(User, auto_now=True)
     
     def __str__(self) -> str:
         return self.user.username
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User)
--- musker/views.py
+++ musker/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Profile 
+from django.contrib import messages
 def home(request):
     return render(request, 'home.html', {})
+
+def profile_list(request):
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user = request.user)
+        return render(request, 'profile_list.html', {"profiles":profiles})
+    else:
+        messages.success(request, ('You Must Be Logged In To View This Page...'))
+        return redirect('home')
