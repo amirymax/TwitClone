a2e6614a99a3a85f23eb1c827502bb27bc77935f
Amiri <amirymax@gmail.com>
2024-10-02 22:36:07 +0300

    Finished 5th lesson

--- musker/views.py
+++ musker/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Profile 
 from django.contrib import messages
+
 def home(request):
     return render(request, 'home.html', {})
 
@@ -8,6 +9,15 @@ def profile_list(request):
     if request.user.is_authenticated:
         profiles = Profile.objects.exclude(user = request.user)
         return render(request, 'profile_list.html', {"profiles":profiles})
+    else:
+        messages.success(request, ('You Must Be Logged In To View This Page...'))
+        return redirect('home')
+
+
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id = pk)
+        return render(request, "profile.html", {"profile":profile})
     else:
         messages.success(request, ('You Must Be Logged In To View This Page...'))
         return redirect('home')
